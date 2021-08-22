49b05b860ad36062b641683104fe56b073824027
AniketDGiri <aniketgiri770@gmail.com>
2021-08-22 09:16:50 +0530

    new changes added

---
+++ scripts/upload_file.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Import modules for CGI handling 
+print ('Content-type:text/html\n')
+print ()
+#print ("Hello World")
+#print("<br>")
+#print(4+10)
+import cgi, os
+import cgitb; cgitb.enable()
+form = cgi.FieldStorage()
+# Get filename here.
+print()
+fileitem = form['filename']
+# Test if the file was uploaded
+if fileitem.filename:
+   # strip leading path from file name to avoid
+   # directory traversal attacks
+   fn = os.path.basename(fileitem.filename.replace("\\", "/" ))
+   open('/var/www/html/3dModels/building/ad/low_poly_building/' + fn, 'wb').write(fileitem.file.read())
+   message = 'The file "' + fn + '" was uploaded successfully'
+else:
+   message = 'No file was uploaded'
+#print ("<html><body><p>%s</p></body></html>" % (message))
+
+
+with open("/var/www/html/ank/html/successpage.html", "r", encoding='utf-8') as f:
+    text=f.read()
+    print(text)
+'''for x in os.listdir():
+    if x.endswith(".html"):
+        # Prints only text file present in My Folder
+        print(x)'''
